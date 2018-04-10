b306817cfd7ac628406848321d026bacbf922f20
Andrew Daniels <skaterphreak@gmail.com>
2018-04-10 13:36:51 -0500

    First commit

---
+++ DanielsAndrew_KraveGymPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DanielsAndrew_KraveGymPrototype
+//
+//  Created by Andrew Daniels on 4/10/18.
+//  Copyright © 2018 Andrew Daniels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
